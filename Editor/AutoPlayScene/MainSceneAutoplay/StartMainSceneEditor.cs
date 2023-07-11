d84a0b5722017ecc4b9858dd97e2f225bd9483ae
david444456 <david-toranzo-@hotmail.com>
2023-07-11 20:02:37 -0300

    Initial commit

---
+++ Editor/AutoPlayScene/MainSceneAutoplay/StartMainSceneEditor.cs
@@ -0,0 +1,102 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityToolbarExtender;
+
+namespace Trantorian.Editor
+{
+  public class StartMainSceneEditor
+  {
+    static class ToolbarStyles
+    {
+      public static readonly GUIStyle commandButtonStyle;
+
+      static ToolbarStyles()
+      {
+        commandButtonStyle = new GUIStyle("Command")
+        {
+          fontSize = 12,
+          alignment = TextAnchor.MiddleCenter,
+          imagePosition = ImagePosition.ImageAbove,
+          fontStyle = FontStyle.Bold,
+          fixedHeight = 20,
+          fixedWidth = 25,
+          richText = true
+        };
+      }
+    }
+
+    [InitializeOnLoad]
+    public class PlayWithMainMenu
+    {
+      static PlayWithMainMenu()
+      {
+        ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+      }
+
+      static void OnToolbarGUI()
+      {
+        if (EditorApplication.isPlaying)
+          return;
+
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button(new GUIContent("<color=white>M</color>", "Load main scene and play"),
+          ToolbarStyles.commandButtonStyle))
+          SceneHelper.StartScene("MainScene", EditorSceneManager.GetActiveScene().name);
+
+      }
+    }
+
+    static class SceneHelper
+    {
+      static string sceneToOpen;
+
+      public static void StartScene(string sceneName, string currentScene)
+      {
+        if (EditorApplication.isPlaying)
+        {
+          EditorApplication.isPlaying = false;
+        }
+
+        sceneToOpen = sceneName;
+        EditorApplication.update += OnExecuteScene;
+      }
+
+      static void OnExecuteScene()
+      {
+        OnUpdate();
+      }
+
+      static void OnUpdate()
+      {
+        if (sceneToOpen == null ||
+            EditorApplication.isPlaying || EditorApplication.isPaused ||
+            EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+          return;
+        }
+
+        EditorApplication.update -= OnUpdate;
+
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+          // need to get scene via search because the path to the scene
+          // file contains the package version so it'll change over time
+          string[] guids = AssetDatabase.FindAssets("t:scene " + sceneToOpen, null);
+          if (guids.Length == 0)
+          {
+            Debug.LogWarning("Couldn't find scene file");
+          }
+          else
+          {
+            string scenePath = AssetDatabase.GUIDToAssetPath(guids[0]);
+            EditorSceneManager.OpenScene(scenePath);
+            EditorApplication.isPlaying = true;
+          }
+        }
+        sceneToOpen = null;
+      }
+    }
+  }
+}
---
+++ Editor/SceneSelector/SceneSelectorListWindow.cs
@@ -0,0 +1,128 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace UtilitiesCustomPackage.EditorExtensions.Windows
+{
+    public class SceneSelectorListWindow : EditorWindow
+    {
+        private Vector2 _scrollPosition;
+        private bool _useMultiScene;
+
+        private string _sceneToSearch = "";
+
+        [MenuItem("Custom Editor/Scene Selector List")]
+        private static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(SceneSelectorListWindow), false, "Scene Selector");
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label("Multiple");
+            _useMultiScene = EditorGUILayout.Toggle("", _useMultiScene);
+
+            GUILayout.EndHorizontal();
+
+            DrawSearchBar();
+            EditorGUILayout.Space(3);
+
+            _scrollPosition = GUILayout.BeginScrollView(_scrollPosition, false, false, GUILayout.Width(this.position.width), GUILayout.MinHeight(1), GUILayout.MaxHeight(1000), GUILayout.ExpandHeight(true));
+
+            DrawAllScenes();
+
+            GUILayout.EndScrollView();
+        }
+
+        private void DrawSearchBar()
+        {
+            GUILayout.BeginHorizontal();
+            _sceneToSearch = GUILayout.TextField(_sceneToSearch);
+            GUILayout.EndHorizontal();
+
+            if (_sceneToSearch != "")
+            {
+                GUILayout.BeginVertical("Box");
+
+                for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+                {
+                    string sceneName = GetSceneName(EditorBuildSettings.scenes[i].path);
+                    sceneName = sceneName.ToLower();
+                    if (!sceneName.Contains(_sceneToSearch.ToLower())) continue;
+
+                    GUILayout.Space(5);
+
+                    if (GUILayout.Button(GetSceneName(EditorBuildSettings.scenes[i].path)))
+                    {
+                        if (_useMultiScene)
+                            EditorSceneManager.OpenScene(EditorBuildSettings.scenes[i].path, OpenSceneMode.Additive);
+                        else
+                        {
+                            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+                            EditorSceneManager.OpenScene(EditorBuildSettings.scenes[i].path);
+                        }
+                    }
+                }
+
+                GUILayout.EndVertical();
+
+                HorizontalRect(2);
+            }
+        }
+
+        private void HorizontalRect(float height)
+        {
+            Rect rect = EditorGUILayout.GetControlRect(false, 10);
+
+            rect.height = height;
+
+            EditorGUI.DrawRect(rect, new Color(0.5f, 0.5f, 0.5f, 1));
+        }
+
+        private void DrawAllScenes()
+        {
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                GUILayout.Space(5);
+
+                if (GUILayout.Button(GetSceneName(EditorBuildSettings.scenes[i].path)))
+                {
+                    if (_useMultiScene)
+                        EditorSceneManager.OpenScene(EditorBuildSettings.scenes[i].path, OpenSceneMode.Additive);
+                    else
+                    {
+                        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+                        EditorSceneManager.OpenScene(EditorBuildSettings.scenes[i].path);
+                    }
+                }
+            }
+        }
+
+        private string GetSceneName(string _path)
+        {
+            char[] path = _path.ToCharArray();
+            string sceneName = "";
+
+            for (int i = path.Length - 1; i >= 0; i--)
+            {
+                if (path[i] == '/')
+                    break;
+                sceneName += path[i];
+            }
+
+            path = sceneName.ToCharArray();
+            sceneName = "";
+
+            for (int i = path.Length - 1; i >= 0; i--)
+            {
+                if (path[i] == '.')
+                    break;
+                sceneName += path[i];
+            }
+
+            return sceneName;
+        }
+    }
+}
